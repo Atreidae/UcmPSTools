98dd13a4bb2f64180026e76c6e03c3f61c424cf7
James Arber <atreidae@chibiko.net>
2021-03-12 16:54:24 +1100

    Added Test and Enable O365 Service cmdlets
    Added two cmdlets for managing enabled and disabled apps in Office365 licenses.

---
+++ Enable-O365Service.ps1
@@ -0,0 +1,104 @@
+﻿Function Enable-O365Service
+{
+	<#
+			.SYNOPSIS
+			Enables a Specified Service in Office365
+
+			.DESCRIPTION
+		
+
+			.EXAMPLE
+			Enable-O365Service -ServiceName MCOEV
+
+			.INPUTS
+			User UPN
+
+			.REQUIRED FUNCTIONS
+			Write-Log: 						https://github.com/Atreidae/PowerShell-Fuctions/blob/main/Write-Log.ps1
+			Write-HTMLReport: 			https://github.com/Atreidae/PowerShell-Fuctions/blob/main/Write-HTMLReport.ps1 (optional)
+
+			.LINK
+			http://www.UcMadScientist.com
+			https://github.com/Atreidae/PowerShell-Fuctions
+
+			.ACKNOWLEDGEMENTS
+
+			.NOTES
+			Version:		1.0
+			Date:			10/03/2021
+
+			.VERSION HISTORY
+			1.0: Initial Public Release
+
+	#>
+
+	Param #No parameters
+	(
+		[Parameter(ValueFromPipelineByPropertyName=$true, Mandatory, Position=1)] $UPN, 
+		[Parameter(ValueFromPipelineByPropertyName=$true, Mandatory, Position=2)] $ServiceName 
+	)
+
+
+	#region FunctionSetup, Set Default Variables for HTML Reporting and Write Log
+	$function = 'Enable-O365Service'
+	[hashtable]$Return = @{}
+	$return.Function = $function
+	$return.Status = "Unknown"
+	$return.Message = "Function did not return a status message"
+
+	# Log why we were called
+	Write-Log -Message "$($MyInvocation.InvocationName) called with $($MyInvocation.Line)" -Severity 1 -Component $function
+	Write-Log -Message "Parameters" -Severity 1 -Component $function -LogOnly
+	Write-Log -Message "$($PsBoundParameters.Keys)" -Severity 1 -Component $function -LogOnly
+	Write-Log -Message "Parameters Values" -Severity 1 -Component $function -LogOnly
+	Write-Log -Message "$($PsBoundParameters.Values)" -Severity 1 -Component $function -LogOnly
+	Write-Log -Message "Optional Arguments" -Severity 1 -Component $function -LogOnly
+	Write-Log -Message "$Args" -Severity 1 -Component $function -LogOnly
+	Write-Host '' #Insert a blank line to make reading output easier on loops
+	
+	#endregion FunctionSetup
+
+	#region FunctionWork
+	#Get the user Licence details
+	$LicenseDetails = (Get-MsolUser -UserPrincipalName $UPN).Licenses
+
+	#Run through all the services on each licence
+	ForEach ($License in $LicenseDetails) {
+		
+		#Find all the Disabled licences and add them to an array, except for the requsted service
+		$DisabledOptions = @()
+		$License.ServiceStatus | ForEach {
+			If ($_.ProvisioningStatus -eq "Disabled" -and $_.ServicePlan.ServiceName -Notlike "*$ServiceName*") #Exclude the requested Service
+			{
+				$DisabledOptions += "$($_.ServicePlan.ServiceName)"
+			}
+		}
+		#Set the Licence options using the new list of disabled licences
+		Try {
+			$LicenseOptions = New-MsolLicenseOptions -AccountSkuId $License.AccountSkuId -DisabledPlans $DisabledOptions
+			Set-MsolUserLicense -UserPrincipalName $UPN -LicenseOptions $LicenseOptions
+			Write-Log -Message "Licence enabled" -Severity 2 -Component $function
+			$Return.Status = "OK"
+			$Return.Message  = "Enabled"
+			Return $Return
+		}
+		Catch
+		{
+			Write-Log -Message "Something went wrong assinging the licence" -Severity 3 -Component $function 
+			$Return.Status = "Error"
+			$Return.Message  = "Unknown Error"
+			Return $Return
+		}
+	} #Repeat for the next Licence
+
+	#endregion FunctionWork
+	#region FunctionReturn
+ 
+	#Default Return Variable for my HTML Reporting Fucntion
+	Write-Log -Message "Reached end of $function without a Return Statement" -Severity 3 -Component $function
+	$return.Status = "Unknown"
+	$return.Message = "Function did not encounter return statement"
+	Return $Return
+	#endregion FunctionReturn
+
+}
---
+++ Test-O365Service.ps1
@@ -0,0 +1,103 @@
+﻿Function Test-O365Service
+{
+	<#
+			.SYNOPSIS
+			Enables a Specified Service in Office365
+
+			.DESCRIPTION
+		
+
+			.EXAMPLE
+			Enable-O365Service -ServiceName MCOEV
+
+			.INPUTS
+			User UPN
+
+			.REQUIRED FUNCTIONS
+			Write-Log: 						https://github.com/Atreidae/PowerShell-Fuctions/blob/main/Write-Log.ps1
+			Write-HTMLReport: 			https://github.com/Atreidae/PowerShell-Fuctions/blob/main/Write-HTMLReport.ps1 (optional)
+
+			.LINK
+			http://www.UcMadScientist.com
+			https://github.com/Atreidae/PowerShell-Fuctions
+
+			.ACKNOWLEDGEMENTS
+
+			.NOTES
+			Version:		1.0
+			Date:			10/03/2021
+
+			.VERSION HISTORY
+			1.0: Initial Public Release
+
+	#>
+
+	Param #No parameters
+	(
+		[Parameter(ValueFromPipelineByPropertyName=$true, Mandatory, Position=1)] $UPN, 
+		[Parameter(ValueFromPipelineByPropertyName=$true, Mandatory, Position=2)] $ServiceName 
+	)
+
+
+	#region FunctionSetup, Set Default Variables for HTML Reporting and Write Log
+	$function = 'Test-O365Service'
+	[hashtable]$Return = @{}
+	$return.Function = $function
+	$return.Status = "Unknown"
+	$return.Message = "Function did not return a status message"
+
+	# Log why we were called
+	Write-Log -Message "$($MyInvocation.InvocationName) called with $($MyInvocation.Line)" -Severity 1 -Component $function
+	Write-Log -Message "Parameters" -Severity 1 -Component $function -LogOnly
+	Write-Log -Message "$($PsBoundParameters.Keys)" -Severity 1 -Component $function -LogOnly
+	Write-Log -Message "Parameters Values" -Severity 1 -Component $function -LogOnly
+	Write-Log -Message "$($PsBoundParameters.Values)" -Severity 1 -Component $function -LogOnly
+	Write-Log -Message "Optional Arguments" -Severity 1 -Component $function -LogOnly
+	Write-Log -Message "$Args" -Severity 1 -Component $function -LogOnly
+	Write-Host '' #Insert a blank line to make reading output easier on loops
+	
+	#endregion FunctionSetup
+
+	#region FunctionWork
+
+	#Get the users licences and enumerate through them looking for the relevant service
+	$LicenseDetails = (Get-MsolUser -UserPrincipalName $UPN).Licenses
+	ForEach ($License in $LicenseDetails) {
+		$License.ServiceStatus | ForEach-Object {
+			If ($_.ProvisioningStatus -eq "Disabled" -and $_.ServicePlan.ServiceName -eq "$serviceName")
+			{ 
+				Write-Log -Message "$ServiceName Disabled" -Severity 2 -Component $function
+				$Return.Status = "Warn"
+				$Return.Message  = "$ServiceName Disabled"
+				Return $Return
+			}
+			Elseif ($_.ProvisioningStatus -eq "Success" -and $_.ServicePlan.ServiceName -eq "$serviceName")
+			{ 
+				Write-Log -Message "$ServiceName Enabled" -Severity 2 -Component $function
+				$Return.Status = "OK"
+				$Return.Message  = "$ServiceName Enabled"
+				Return $Return
+			}
+			ElseIf ($_.ServicePlan.ServiceName -like "*$serviceName*")
+			{
+				Write-Log -Message "$ServiceName is in unknown state" -Severity 2 -Component $function
+				$Return.Status = "ERROR"
+				$Return.Message  = "$ServiceName is in unknown state"
+				Return $Return
+			}
+
+		}
+	}
+
+	#endregion FunctionWork
+
+	#region FunctionReturn
+ 
+	#Default Return Variable for my HTML Reporting Fucntion
+	Write-Log -Message "Reached end of $function without a Return Statement" -Severity 3 -Component $function
+	$return.Status = "Unknown"
+	$return.Message = "Function did not encounter return statement"
+	Return $Return
+	#endregion FunctionReturn
+
+}
