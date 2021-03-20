28b4ad6f3c45bcc6677a27e8f65516dce48017f4
James Arber <atreidae@chibiko.net>
2021-03-20 11:06:51 +1100

    updates for azure build

--- build_scripts/build.ps1
+++ build_scripts/build.ps1
@@ -4,21 +4,19 @@
 $buildVersion = $env:BUILDVER
 $moduleName = 'UcmPSTools'
 
-## Find the module manifest while running on the build agent
 $manifestPath = Join-Path -Path $env:SYSTEM_DEFAULTWORKINGDIRECTORY -ChildPath "$moduleName.psd1"
 
-## Update version in the manifest to the current build
+## Update build version in manifest
 $manifestContent = Get-Content -Path $manifestPath -Raw
 $manifestContent = $manifestContent -replace '<ModuleVersion>', $buildVersion
 
-## Find all of the public functions and create a string for the manifest
+## Find all of the public functions
 $publicFuncFolderPath = Join-Path -Path $PSScriptRoot -ChildPath 'public'
 if ((Test-Path -Path $publicFuncFolderPath) -and ($publicFunctionNames = Get-ChildItem -Path $publicFuncFolderPath -Filter '*.ps1' | Select-Object -ExpandProperty BaseName)) {
 	$funcStrings = "'$($publicFunctionNames -join "','")'"
 } else {
 	$funcStrings = $null
 }
-
 ## Add all public functions to FunctionsToExport attribute
 $manifestContent = $manifestContent -replace "'<FunctionsToExport>'", $funcStrings
 $manifestContent | Set-Content -Path $manifestPath
