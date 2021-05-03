f24d8147426e283936f55cc8b86f40ee4eee71dd
James Arber <atreidae@chibiko.net>
2021-05-03 17:13:53 +1000

    Update UcmPSTools.Tests.ps1
    Fix pester test path issues

--- UcmPSTools.Tests.ps1
+++ UcmPSTools.Tests.ps1
@@ -6,15 +6,15 @@ Install-Module -Name PSScriptAnalyzer -Force
 describe 'Module-level tests' {
 
 	it 'the module imports successfully' {
-		{ Import-Module -Name "$PSScriptRoot\PowerShellModuleProject.psm1" -ErrorAction Stop } | should not throw
+		{ Import-Module -Name "$PSScriptRoot\UcmPSTools.psm1" -ErrorAction Stop } | should not throw
 	}
 
 	it 'the module has an associated manifest' {
-		Test-Path "$PSScriptRoot\PowerShellModuleProject.psd1" | should -Be $true
+		Test-Path "$PSScriptRoot\UcmPSTools.psd1" | should -Be $true
 	}
 
 	it 'passes all default PSScriptAnalyzer rules' {
-		Invoke-ScriptAnalyzer -Path "$PSScriptRoot\PowerShellModuleProject.psm1" | should -BeNullOrEmpty
+		Invoke-ScriptAnalyzer -Path "$PSScriptRoot\UcmPSTools.psm1" | should -BeNullOrEmpty
 	}
 
 }
