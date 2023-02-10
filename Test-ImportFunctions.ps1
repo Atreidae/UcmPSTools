2dd640dd925f40d2137ca6771bbf9ac6db021a1b
James Arber <atreidae@chibiko.net>
2023-02-10 19:20:48 +1100

    TeamsConfigFramwork
    Starting Dev on Import-CsTeamsConfig for Auditing.

--- Test-ImportFunctions.ps1
+++ Test-ImportFunctions.ps1
@@ -1,40 +1,40 @@
 <#
-		.SYNOPSIS
-		This powershell script attempts to dot source all the modules in this repository.
+    .SYNOPSIS
+    This powershell script attempts to dot source all the modules in this repository.
 
-		.DESCRIPTION
-		Returns $True if no error was found
+    .DESCRIPTION
+    Returns $True if no error was found
 
-		.EXAMPLE
-		Test-Importfunctions
+    .EXAMPLE
+    Test-Importfunctions
 
-		.INPUTS
-		None
+    .INPUTS
+    None
 
-		.REQUIRED FUNCTIONS
-		None
+    .REQUIRED FUNCTIONS
+    None
 
-		.LINK
-		http://www.UcMadScientist.com
-		https://github.com/Atreidae/UcmPSTools
+    .LINK
+    http://www.UcMadScientist.com
+    https://github.com/Atreidae/UcmPSTools
 
-		.ACKNOWLEDGEMENTS
-		Thanks to Adam the Automater for getting me started on Pipeline automation.
-		Check out https://adamtheautomator.com/powershell-devops/ for more info!
+    .ACKNOWLEDGEMENTS
+    Thanks to Adam the Automater for getting me started on Pipeline automation.
+    Check out https://adamtheautomator.com/powershell-devops/ for more info!
 
-		.NOTES
-		Version:		1.0
-		Date:			14/05/2021
+    .NOTES
+    Version:		1.0
+    Date:			14/05/2021
 
-		.VERSION HISTORY
-		1.0: Initial Public Release
+    .VERSION HISTORY
+    1.0: Initial Public Release
 
 #>
 
-	Param #No parameters
-	(
-		[Parameter(ValueFromPipelineByPropertyName=$true, Position=1)] [switch]$Private
-	)
+  Param #No parameters
+  (
+    [Parameter(ValueFromPipelineByPropertyName=$true, Position=1)] [switch]$Private
+  )
 
 $Global:LogFileLocation = $PSCommandPath -replace '.ps1','.log'
 
@@ -45,65 +45,66 @@ $publicFuncFolderPath = Join-Path -Path $PSScriptRoot -ChildPath 'public'
 #Check there is actually files here
 if ((Test-Path -Path $publicFuncFolderPath) -and ($publicFunctionNames = Get-ChildItem -Path $publicFuncFolderPath -Filter '*.ps1' | Select-Object PsPath))
 {
-	#Run through each file to import
-	ForEach ($FunctionName in $publicFunctionNames)
-	{
-		#Import the fucntion
-		Try
-		{ 
-			Write-host "Importing $FunctionName"
-			.$FunctionName.PsPath
-		}
+  #Run through each file to import
+  ForEach ($FunctionName in $publicFunctionNames)
+  {
+    #Import the fucntion
+    Try
+    { 
+      Write-host "Importing $FunctionName"
+      .$FunctionName.PsPath
+    }
 
-		#Error during import
-		Catch
-		{
-		Write-Error "Error importing $FunctionName"
-		Return $False
-		}
-	}
+    #Error during import
+    Catch
+    {
+      Write-Error "Error importing $FunctionName"
+      Return $False
+    }
+  }
 
 }
 #No files to import
 else
 {
-	Write-Warning "No Public Modules to import, try with the Private flag set"
+  Write-Warning "No Public Modules to import, try with the Private flag set"
 }
 
 #Include importing the private functions
 If ($private)
 {
-## Find all of the private functions
-$publicFuncFolderPath = Join-Path -Path $PSScriptRoot -ChildPath 'private'
+  ## Find all of the private functions
+  $publicFuncFolderPath = Join-Path -Path $PSScriptRoot -ChildPath 'private'
 
-#Check there is actually files here
-if ((Test-Path -Path $publicFuncFolderPath) -and ($publicFunctionNames = Get-ChildItem -Path $publicFuncFolderPath -Filter '*.ps1' | Select-Object PsPath))
-{
-	#Run through each file to import
-	ForEach ($FunctionName in $publicFunctionNames)
-	{
-		#Import the fucntion
-		Try
-		{ 
-			Write-host "Importing $FunctionName"
-			.$FunctionName.PsPath
-		}
+  #Check there is actually files here
+  if ((Test-Path -Path $publicFuncFolderPath) -and ($publicFunctionNames = Get-ChildItem -Path $publicFuncFolderPath -Filter '*.ps1' | Select-Object PsPath))
+  {
+    #Run through each file to import
+    ForEach ($FunctionName in $publicFunctionNames)
+    {
+      #Import the function
+      Try
+      { 
+        Write-host "Importing $FunctionName"
+        .$FunctionName.PsPath
+      }
 
-		#Error during import
-		Catch
-		{
-		Write-Error "Error importing $FunctionName"
-		Return $False
-		}
-	}
+      #Error during import
+      Catch
+      {
+        Write-Error "Error importing $FunctionName"
+        Return $False
+      }
+    }
 
-}
-#No files to import
-else
-{
-	Write-Warning "No Private Modules to import"
-}
+  }
+  #No files to import
+  else
+  {
+    Write-Warning "No Private Modules to import"
+  }
 }
 
 #Return true if we didnt catch anything
+$global:UcmPsToolsLoaded = $true
 Return $True
---
+++ private/Import-CsTeamsConfiguration.ps1
@@ -0,0 +1,136 @@
+﻿#PerformScriptSigning
+Function Import-CsTeamsConfiguration
+{
+  <#
+      .SYNOPSIS
+      Function to scrape relevant Unified Communications Teams settings and dump them into a zip file
+
+      .DESCRIPTION
+      An Auditing tool, The tool will run through all the relevant Teams settings for Calling and output them into a zip file in the local folder
+      Useful with my Get-CsTeamsConfigToWord script
+
+      .EXAMPLE
+      Import-CsTeamsConfiguration
+
+      .PARAMETER ZipFileName
+      (Optional) Defaults to TeamsConfigReport.zip
+
+      .INPUTS
+      This function does not accept any inputs
+
+      .REQUIRED FUNCTIONS/MODULES
+      Modules
+      UcmPSTools											(Install-Module UcmPsTools) Includes Cmdlets below.
+
+      Cmdlets
+      Write-UcmLog: 									https://github.com/Atreidae/UcmPsTools/blob/main/public/Write-UcmLog.ps1
+      Write-HTMLReport: 							https://github.com/Atreidae/UcmPsTools/blob/main/public/Write-HTMLReport.ps1 (optional)
+	
+      .REQUIRED PERMISSIONS
+      'CSReadOnlyAdministrator' or better
+
+      .LINK
+      https://www.UcMadScientist.com
+      https://github.com/Atreidae/UcmPSTools
+
+
+      .NOTES
+      Version:		0.1
+      Date:			21/10/2022
+
+      .VERSION HISTORY
+      0.1: Initial Beta Release
+
+      .THANKS
+      Special thanks go out to "GreigInSydney" (Greig Sheriden - https://greiginsydney.com/ ) and "EmptyMessage" ( Chris Cook - https://emptymessage.com/ ) for their tools that inspired this one
+  #>
+
+  Param
+  (
+    [Parameter(Position=1)] $ZipFileName="TeamsConfigReport.zip"
+  )
+
+  #region FunctionSetup, Set Default Variables for HTML Reporting and Write Log
+  $function = 'Import-CsTeamsConfiguration'
+  [hashtable]$Return = @{}
+  $return.Function = $function
+  $return.Status = "Unknown"
+  $return.Message = "Function did not return a status message"
+
+  # Log why we were called
+  Write-UcmLog -Message "$($MyInvocation.InvocationName) called with $($MyInvocation.Line)" -Severity 1 -Component $function
+  Write-UcmLog -Message "Parameters" -Severity 1 -Component $function -LogOnly
+  Write-UcmLog -Message "$($PsBoundParameters.Keys)" -Severity 1 -Component $function -LogOnly
+  Write-UcmLog -Message "Parameters Values" -Severity 1 -Component $function -LogOnly
+  Write-UcmLog -Message "$($PsBoundParameters.Values)" -Severity 1 -Component $function -LogOnly
+  Write-UcmLog -Message "Optional Arguments" -Severity 1 -Component $function -LogOnly
+  Write-UcmLog -Message "$Args" -Severity 1 -Component $function -LogOnly
+  Write-Host '' #Insert a blank line to make reading output easier on loops
+	
+  #endregion FunctionSetup
+
+  #region FunctionWork
+
+  Write-UcmLog -Message "Checking for relevant connections" -Severity 2 -Component $function
+  
+  #todo Check Connections
+  
+
+  #Global Variables
+
+  #Now create an empty "Report Variable"
+
+  $global:CsTeamConfig=@()
+  #Todo init report
+
+
+
+  #AppPolicies
+  
+  Get-CsApplicationAccessPolicy 
+
+
+
+
+
+  #Teams Apps and special config
+  
+  #look for non standard apps
+  
+  get-teamsapp | Where {$_.DistributionMethod -ne "Store"}
+
+
+
+
+  #Team Info
+  Get-Team
+  
+  #foreach team
+  Get-TeamChannel
+  
+  ## Get Team user details
+  Get-TeamUser a6601176-d6fc-4295-b6fa-6c3c0304c799
+  
+  ##Check for installed apps
+  Get-TeamsAppInstallation a6601176-d6fc-4295-b6fa-6c3c0304c799
+  
+  #foreach channel
+  Get-TeamChannelUser
+  
+    
+    
+    
+    
+  
+  #Voice Apps
+  
+  
+  
+  
+  #For each Object
+  Export-CsAutoAttendantHolidays
+    
+  
+  
+  }#End of main function
+
