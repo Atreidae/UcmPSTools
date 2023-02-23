e6f1a61fb4d2bd90e4494557446fea0d61313edf
James Arber <atreidae@chibiko.net>
2023-02-23 16:16:03 +1100

    Cache PSScriptAnalyzer

--- build_scripts/build.ps1
+++ build_scripts/build.ps1
@@ -14,7 +14,7 @@ $manifestContent = Get-Content -Path $manifestPath -Raw
 $manifestContent = $manifestContent -replace '<ModuleVersion>', $buildVersion
 
 ## Find all of the public functions
-$publicFuncFolderPath = Join-Path -Path $PSScriptRoot -ChildPath 'public'
+$publicFuncFolderPath = Join-Path -Path $manifestPath -ChildPath 'public'
 if ((Test-Path -Path $publicFuncFolderPath) -and ($publicFunctionNames = Get-ChildItem -Path $publicFuncFolderPath -Filter '*.ps1' | Select-Object -ExpandProperty BaseName)) {
 	$funcStrings = "'$($publicFunctionNames -join "','")'"
 } else {
@@ -23,3 +23,5 @@ if ((Test-Path -Path $publicFuncFolderPath) -and ($publicFunctionNames = Get-Chi
 ## Add all public functions to FunctionsToExport attribute
 $manifestContent = $manifestContent -replace "'<FunctionsToExport>'", $funcStrings
 $manifestContent | Set-Content -Path $manifestPath
+
+##Sign all the PS1 files
