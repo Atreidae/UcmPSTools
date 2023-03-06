c1882ae740212b4cc708fdb2852402c473906fb1
James Arber <atreidae@chibiko.net>
2023-03-06 21:33:22 +1100

    Update build.ps1

--- build_scripts/build.ps1
+++ build_scripts/build.ps1
@@ -3,11 +3,11 @@
 #Plus Nicola Suter's guide to signing PowerShell scripts using Aure Pipelines https://tech.nicolonsky.ch/sign-powershell-az-devops/ and more recently GitHub https://tech.nicolonsky.ch/github-actions-powershell-signing/
 #and COLIN DEMBOVSKY's guide to Azure Pipeline Variables https://colinsalmcorner.com/azure-pipeline-variables/
 
-$buildVersion = {(get-content ./build_scripts/version) + ".$($ENV:build)"}
+$buildVersion = {(get-content ./version) + ".$($ENV:build)"}
 Write-Debug "Building version $buildversion"
 $moduleName = 'UcmPSTools'
 
-$manifestPath = Join-Path -Path $PSScriptRoot -ChildPath "$moduleName.psd1"
+$manifestPath = Join-Path -Path $PSScriptRoot -ChildPath "..\$moduleName.psd1"
 
 ## Update build version in manifest
 $manifestContent = Get-Content -Path $manifestPath -Raw
